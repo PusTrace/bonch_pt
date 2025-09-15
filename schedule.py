f717105330839c054b6f3fa36c79ad4535f61d80
PusTrace <sergeivavilin2005@mail.ru>
2025-09-15 15:02:56 +0300

    start fix schedule

--- schedule.py
+++ schedule.py
@@ -19,7 +19,7 @@ def push_schedule_to_db(cursor, entry):
     cursor.execute(
         """
         INSERT INTO schedule (date, pair, subject, auditorium, teacher, lesson_type, sect)
-        VALUES (%s, %s, %s, %s, %s, %s, 'IKB-32')
+        VALUES (%s, %s, %s, %s, %s, %s, 'IKB-31')
         ON CONFLICT (date, pair) DO NOTHING
         """,
         entry
@@ -32,17 +32,32 @@ def parse_bonch(cursor, current_date, duration):
     for week in range(duration):
         week_start = current_date - timedelta(days=current_date.weekday())  
         url_date = week_start.strftime('%Y-%m-%d')
-        url = f"https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya?group=56161&date={url_date}"
+        url = f"https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya?group=56160&date={url_date}"
+
+        cookies = {
+            "PHPSESSID": "b524092011e6cd2d9b22e7eb18063e55"
+        }
+
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Dnt": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
             "Referer": "https://www.sut.ru/",
         }
-        response = requests.get(url, headers=headers)
+
+        response = requests.get(url, headers=headers, cookies=cookies)
+        
         if response.status_code != 200:
             print(f"Ошибка {response.status_code} при загрузке {url}")
             continue
 
+        print(response.text)
         soup = BeautifulSoup(response.text, 'html.parser')
 
         
@@ -59,9 +74,31 @@ def parse_bonch(cursor, current_date, duration):
                 auditorium = block.find("div", {"class": "vt242"})
                 lesson_type = block.find("div", {"class": "vt243"})
 
-                if not subject or not teacher or not auditorium or not lesson_type:
+                print(day_date,
+                    pairs[pair_index] if pair_index < len(pairs) else None,
+                    subject.get_text(strip=True) if subject is not None else None,
+                    auditorium.get_text(strip=True) if auditorium is not None else None,
+                    teacher.get_text(strip=True) if teacher is not None else None,
+                    lesson_type.get_text(strip=True) if lesson_type is not None else None)
+                
+                if not all([subject, teacher, auditorium, lesson_type]):
+                    print("Пропущено:")
+                    if not subject:
+                        print("  ❌ Нет предмета")
+                    if not teacher:
+                        print("  ❌ Нет преподавателя")
+                    if not auditorium:
+                        print("  ❌ Нет аудитории")
+                    if not lesson_type:
+                        print("  ❌ Нет типа занятия")
                     pair_index += 1
-                    continue
+                    
+                    a = input("Press Enter to continue: ")
+                    if a.strip() == "":
+                        continue
+                    else:
+                        quit()
+
 
                 entry = (
                     day_date,
@@ -82,14 +119,15 @@ def parse_bonch(cursor, current_date, duration):
 def last_date_from_db(cursor):
     cursor.execute("SELECT MAX(date) FROM schedule WHERE sect='IKB-32'")
     result = cursor.fetchone()
-    return result[0].date() if result[0] else datetime.now().date()
+    return result[0].date() if result[0] else datetime.strptime("2025-09-01", "%Y-%m-%d").date()
+
 
 if __name__ == "__main__":
     load_dotenv()
     password = os.getenv("DB_PASSWORD")
     conn, cursor = connect_db(password)
     current_date = last_date_from_db(cursor)
-    duration = 9
+    duration = 1
     parse_bonch(cursor, current_date, duration)
     conn.commit()
     conn.close()
